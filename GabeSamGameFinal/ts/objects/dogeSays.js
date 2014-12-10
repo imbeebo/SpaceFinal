cd6f10e0778c5a17a9c9f3da2f2c580bd9bb7c06
imbeebo <thegeekforgod@gmail.com>
2014-12-10 15:40:06 -0500

    removed uncommented/unused code

--- GabeSamGameFinal/ts/objects/dogeSays.js
+++ GabeSamGameFinal/ts/objects/dogeSays.js
@@ -1,24 +1,8 @@
-﻿
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*
 File: Button.ts
 Author: Gabriel Hounsome & Sam Halloran
 Website: Meme Wars
-Description: This is a side-scroller shooter game 
+Description: This is a side-scroller shooter game
 where the player must shoot oncoming memes. Avoid being hit or shot by them. There are powerups to help you get through the levels
 
 
@@ -29,15 +13,4 @@ Date Last Modified: November 07, 2014
 Citations: Used JQuery, collsion detection module from indiegamer and royalty free art from: wrathgames.com
 Royalty Free Music: royalty free music from: http://www.looperman.com/media/loops/630386/looperman-l-0630386-0077610-mrfunktastic-trap-gods-bells-140f.mp3
 */
-
-//module objects {
-//    // Button Class
-//    export class CaptionShooter {
-//        private dogeMemes = ["So Destroy", "Very Violence", "Much Fight", "Very Wow",
-//            "very mlg", "wow", "many cat", "much coin", "WOW", "very doge",
-//            "many side scroller", "much wow", "so advanced web", "much cool",
-//            "many premium", "such doge"];
-//        getDogeMeme() {
-//        }
-//    }
-//}   
+//# sourceMappingURL=dogeSays.js.map
